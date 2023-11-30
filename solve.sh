3338c133328c733c5f0e4ee50410cadc38e6c5a7
Bernie Gray <bfgray3@gmail.com>
2023-11-29 20:28:27 -0500

    fix solve.sh usage

--- solve.sh
+++ solve.sh
@@ -10,6 +10,6 @@ if [[ $# -eq 1 ]]
 then
   get_answer "$1"/* "./$(cut -d/ -f 1 <<< "$1")/input.txt"
 else
-  echo "Usage: ./solve.sh [<dd>/{a,b}/]" >&2
+  echo "Usage: ./solve.sh <dd>/{a,b}/" >&2
   exit 1
 fi
